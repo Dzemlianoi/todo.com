87404e845a84ec5795b18e45e1aa4ac0bef44586
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-08-03 13:18:37 +0300

    update migration: create_project

--- migrations/m160803_093033_create_project.php
+++ migrations/m160803_093033_create_project.php
@@ -6,24 +6,16 @@ class m160803_093033_create_project extends Migration
 {
     public function up()
     {
-
+        $this->createTable('projects',[
+            'id'=>$this->primaryKey(),
+            'name'=>$this->string(40)->unique()->notNull(),
+        ]);
     }
 
     public function down()
     {
-        echo "m160803_093033_create_project cannot be reverted.\n";
 
-        return false;
-    }
+        $this->dropTable('projects');
 
-    /*
-    // Use safeUp/safeDown to run migration code within a transaction
-    public function safeUp()
-    {
     }
-
-    public function safeDown()
-    {
-    }
-    */
 }
