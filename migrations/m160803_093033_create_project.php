8a29e51dd97e58b81fc592aa46cb3fef85574c7f
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-08-03 12:31:57 +0300

    initialize 3 migrations: - create_user - create_project - create_task

---
+++ migrations/m160803_093033_create_project.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160803_093033_create_project extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160803_093033_create_project cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
---
+++ migrations/m160803_093041_create_task.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160803_093041_create_task extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160803_093041_create_task cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
