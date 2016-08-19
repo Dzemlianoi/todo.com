71eb1969fc5735b05e547c298b1d3112c23b9c14
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-08-19 15:21:03 +0300

    Added AuthController and view of Auth

---
+++ views/auth/index.php
@@ -0,0 +1,14 @@
+<div class="container-fluid form-box">
+    <form role="form" action="http://questinarium/auth/login" method="post">
+        <div class="form-group admin-form-group">
+            <label for="email">Put your login</label>
+            <input type="text" class="form-control" id="login" name="login" placeholder="Put your login">
+        </div>
+        <div class="form-group admin-form-group">
+            <label for="pass">Password</label>
+            <input type="password" class="form-control" id="pass" name="password" placeholder="Put your password">
+        </div>
+
+        <button type="submit" class="btn btn-primary admin-submit">Enter</button>
+    </form>
+</div>
