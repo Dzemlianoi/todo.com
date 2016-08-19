db51d3b9d818b6a9501320ca224355cc600faf9f
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-08-19 15:17:52 +0300

    changed assets pathes of css & js

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,12 +18,14 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/style.css',
     ];
     public $js = [
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
 }
