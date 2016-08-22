daa0abadc9b0bde34e63930851ad0ca148ed7319
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-08-22 18:37:56 +0300

    Updated layout

--- views/layouts/auth.php
+++ views/layouts/auth.php
@@ -1,5 +1,7 @@
 <?php
 use \app\assets\AppAsset;
+use \yii\bootstrap\NavBar;
+use \yii\bootstrap\Nav;
 
 /*@var $content string
  *@var $this \yii\web\View */
@@ -9,61 +11,62 @@ $this->beginPage();
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="<?= Yii::$app->language ?>">
 <head>
-<?php
-    $this->head();
-?>
+    <meta charset="<?= Yii::$app->charset?>"/>
+    <?= $this->head() ?>
 </head>
 <body>
+<?=$this->beginBody()?>
 <div class="wrapper container admin-wrapper">
-    <nav class="navbar navbar-light navbar-full bg-primary navbar-admin">
+    <?php
+    NavBar::begin(
+        [
+            'brandLabel' => 'Ruby garage ToDo',
+            'options' => ['class'=>'custom-navbar']
+        ]
+    );
 
-        <div class="row nav-row">
-            <a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/' ?>">
-                <div class="inline navbar-brand m-b-0 admin-logo">Questionarium</div>
-            </a>
+    echo Nav::widget([
+        'items' => [
+            [
+                'label' => 'Home',
+                'url' => ['/auth/index']
+            ],
+            [
+                'label' => 'About',
+                'url' => ['/auth/about']
+            ],
+            [
+                'label' => 'Sign in',
+                'url' => ['/auth/signin']
+            ],
+            [
+                'label' => 'Register',
+                'url' => ['/auth/register']
+            ],
+        ],
+        'options' => ['class' => 'navbar-nav'],
+    ]);
 
-            <a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/admin' ?>">
-                <div class="inline navbar-item active">
-                    Admin
-                    <i class="glyphicon glyphicon-home"></i>
-                </div>
-            </a>
 
-            <div class="inline navbar-item">
-                About
-                <i class="glyphicon glyphicon-question-sign"></i>
-            </div>
-            <?php
-            //ВЫНЕСТИ НАФИГ ОТСЮДА!!!
-            if (!empty ($_SESSION['user'])) {
-                $link = 'http://' . $_SERVER['HTTP_HOST'] . '/auth/exit';
-                $text='Sign out';
-                $glyph='glyphicon-log-out';
-            }else{
-                $link = 'http://' . $_SERVER['HTTP_HOST'] . '/auth';
-                $text='Sign in';
-                $glyph='glyphicon-log-in';
-            }
-            //ПОВТОРЮСЬ - НАФИГ!!!
-            ?>
-            <a href="<?php echo $link?>">
-                <div class="inline navbar-item">
-                    <?php echo $text ?>
-                    <i class="glyphicon <?php echo $glyph ?>"></i>
-                </div>
-            </a>
-            <div class="inline navbar-item">
-                <span class="navbar-span">Contacts</span>
-                <i class="glyphicon glyphicon-facetime-video"></i>
-            </div>
-        </div>
-    </nav>
+    NavBar::end() ?>
+<!--    Insert content-->
+    <div class="container">
+        <?=$content ?>
+    </div>
 
-    <?php $content_view ?>
 
 </div>
+<footer class="footer">
+    <div class="container">
+        <span class="badge">
+            <span class="glyphicon glyphicon-copyright-mark"></span>Dzemlianoi<?=date('Y-M')?>
+        </span>
+    </div>
+</footer>
+
+<?=$this->endBody()?>
 </body>
 </html>
 <?php
