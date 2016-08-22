b7a764431937313a4dc6595ace7b45a0c41aa94a
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-08-22 22:28:25 +0300

    added 2 actions - Registartion, Signin

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,6 +2,10 @@
 
 namespace app\controllers;
 
+use Yii;
+use app\models\RegForm;
+use app\models\LoginForm;
+
 class AuthController extends \yii\web\Controller
 {
     public function actionIndex()
@@ -23,23 +27,33 @@ class AuthController extends \yii\web\Controller
             ]);
     }
 
-    public function actionSignin()
-    {
-        if (!empty ($_SESSION['user'])) {
-            $link = 'http://' . $_SERVER['HTTP_HOST'] . '/auth/exit';
-            $text='Sign out';
-            $glyph='glyphicon-log-out';
-        }else{
-            $link = 'http://' . $_SERVER['HTTP_HOST'] . '/auth';
-            $text='Sign in';
-            $glyph='glyphicon-log-in';
+    public function actionSignin(){
+        $model=new LoginForm();
+        if ($model->load(Yii::$app->request->post())&&$model->signin()){
+            return $this->goHome();
         }
-        return $this->render('index',
-            [
-                'link'=>$link,
-                'text'=>$text,
-                'glyph'=>$glyph
-            ]);
+        return $this->render(
+            'signin',
+            ['model'=>$model]
+        );
+    }
+
+    public function actionRegistration(){
+
+        $model=new RegForm();
+        if ($model->load(Yii::$app->request->post())&&$model->validate()){
+            if ($model->reg()){
+                return $this->goHome();
+            }else{
+                Yii::$app->session->setFlash('message','Something goes wrong, try again later');
+                Yii::error('Registration error'.'<pre>'.Yii::$app->request->post().'</pre>');
+                return $this->actionRegistration();
+            }
+        }
+        return $this->render(
+            'registration',
+            ['model'=>$model]
+            );
     }
 
 }
