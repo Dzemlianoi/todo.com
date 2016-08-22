9b89978395c5e7649a34f20a460cac39521a6f7e
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-08-22 18:40:29 +0300

    controller update

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -6,7 +6,40 @@ class AuthController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        if (!empty ($_SESSION['user'])) {
+            $link = 'http://' . $_SERVER['HTTP_HOST'] . '/auth/exit';
+            $text='Sign out';
+            $glyph='glyphicon-log-out';
+        }else{
+            $link = 'http://' . $_SERVER['HTTP_HOST'] . '/auth';
+            $text='Sign in';
+            $glyph='glyphicon-log-in';
+        }
+        return $this->render('index',
+            [
+                'link'=>$link,
+                'text'=>$text,
+                'glyph'=>$glyph
+            ]);
+    }
+
+    public function actionSignin()
+    {
+        if (!empty ($_SESSION['user'])) {
+            $link = 'http://' . $_SERVER['HTTP_HOST'] . '/auth/exit';
+            $text='Sign out';
+            $glyph='glyphicon-log-out';
+        }else{
+            $link = 'http://' . $_SERVER['HTTP_HOST'] . '/auth';
+            $text='Sign in';
+            $glyph='glyphicon-log-in';
+        }
+        return $this->render('index',
+            [
+                'link'=>$link,
+                'text'=>$text,
+                'glyph'=>$glyph
+            ]);
     }
 
 }
