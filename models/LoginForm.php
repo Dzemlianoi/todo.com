7219b321ba974b450373682c3e4338bcf60e4644
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-08-25 10:40:09 +0300

    user validation updated

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -4,12 +4,14 @@ namespace app\models;
 
 use Yii;
 use yii\base\Model;
+use yii\web\User;
 
 class LoginForm extends Model{
     public $login;
     public $password;
     public $email;
     public $rememberMe=1;
+    private $_user=false;
 
     public function rules(){
         return
@@ -23,8 +25,22 @@ class LoginForm extends Model{
 
             ];
     }
-
+    public function getUser(){
+        if ($this->_user===false){
+            $this->_user=Users::findIdentityByLogin($this->login);
+        }
+        return $this->_user;
+    }
     public function validatePassword($attribute){
+        $user=$this->getUser();
+        if (!$user){
+            $this->addError($attribute,'Such login doesnt exist');
+        }else if(!$user->validatePassword($this->password)) {
+            $this->addError($attribute, 'Wrong password');
+        }else{
+            return true;
+        }
+
 
     }
 
@@ -38,6 +54,10 @@ class LoginForm extends Model{
     }
 
     public function signin(){
-        return true;
+
+        $user=$this->getUser();
+
+        return $this->validate()?Yii::$app->user->login($user,$this->rememberMe?3600*24*30:0):false;
+
     }
 }
