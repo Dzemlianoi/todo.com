7efd78d5f3444381fdc35c018e9fab5c6c90bb53
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-08-25 10:42:02 +0300

    update regform - now pass can be confirmed

--- models/RegForm.php
+++ models/RegForm.php
@@ -9,27 +9,40 @@ class RegForm extends Model{
     public $login;
     public $password;
     public $email;
+    public $confirmPassword;
 
     public function rules(){
         return[
-            [['login','email','password'], 'required'],
+            [['login','email','password','confirmPassword'], 'required'],
             [['login','email','password'],'filter','filter'=>'trim'],
             ['login','string','min'=>'2','max'=>'42'],
-            ['password','string','min'=>'6','max'=>'32'],
+            [['password','confirmPassword'],'string','min'=>'6','max'=>'32'],
             ['login','unique','targetClass'=>Users::className(),'message'=>'This username isnt unique'],
             ['email','unique','targetClass'=>Users::className(),'message'=>'This email isnt unique'],
             ['email','email'],
         ];
     }
+    public function attributeLabels()
+    {
+       return [
+          'confirmPassword'=>'Confirm password'
+        ];
+    }
+
     public function reg(){
         $user = new Users();
         $user->login=$this->login;
         $user->email=$this->email;
         $user->generateAuthKey();
         $user->setPassword($this->password);
-
-        return $user->save()?$user:NULL;
+        if ($this->checkConfirmation()){
+            return $user->save()?$user:NULL;
+        }
     }
 
+    public function checkConfirmation(){
 
+        return $this->password==$this->confirmPassword;
+
+    }
 }
