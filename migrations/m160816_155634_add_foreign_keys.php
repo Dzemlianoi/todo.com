07e7b05b12145ddbd5b004eba858433ae09bb471
Denis <dzemlianoi>
2016-08-29 15:41:21 +0300

    Cascade deleting of Projects

--- migrations/m160816_155634_add_foreign_keys.php
+++ migrations/m160816_155634_add_foreign_keys.php
@@ -7,7 +7,7 @@ class m160816_155634_add_foreign_keys extends Migration
     public function up()
     {
         $this->addForeignKey('user_id','projects','user_id', 'users','id');
-        $this->addForeignKey('project_id','tasks','project_id','projects','id');
+        $this->addForeignKey('project_id','tasks','project_id','projects','id','CASCADE');
     }
 
     public function down()
