b7f22b26fe139931a55f8d7584bf79f2aaed637c
Denis <dzemlianoi>
2016-08-30 12:22:06 +0300

    new configures for prod

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=todo',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=u383223401_todo',
+    'username' => 'u383223401_root',
+    'password' => 'dontaskwhy911',
     'charset' => 'utf8',
 ];
